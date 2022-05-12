fad852b35a4b78679550ecd7bdf970f2ab55391d
Andrew^-^ <99921755+Maestro2315@users.noreply.github.com>
2022-05-12 21:19:35 +0400

    bad

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        StartMenu startMenu = new StartMenu();
+        startMenu.startMenuService();
+        System.out.println(startMenu);
+
+
+    }
+
+}
